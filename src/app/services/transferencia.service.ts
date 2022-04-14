d7aebc139b996bc49749012a969671a2cc88e5ce
Emerson Almeida <emerson.almeida@adviseu.com.br>
2022-04-14 09:49:22 -0300

    Fazendo a lógica da transferência de exibir na tela e de fazer uma nova requisição de transferência

--- src/app/services/transferencia.service.ts
+++ src/app/services/transferencia.service.ts
@@ -1,3 +1,6 @@
+import { Observable } from 'rxjs';
+import { Transferencia } from '../../models/transferencia.models';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,8 +8,9 @@ import { Injectable } from '@angular/core';
 })
 export class TransferenciaService {
   private listaTransferencia: any[];
+  private url = 'http://localhost:3000/transferencias';
 
-  constructor() {
+  constructor(private httpClient: HttpClient) {
     this.listaTransferencia = [];
   }
 
@@ -14,8 +18,14 @@ export class TransferenciaService {
     return this.listaTransferencia;
   }
 
-  adicionar(transferencia: any) {
+  todas(): Observable<Transferencia[]> {
+    return this.httpClient.get<Transferencia[]>(this.url);
+  }
+
+  adicionar(transferencia: Transferencia): Observable<Transferencia> {
     this.hidratar(transferencia);
+
+    return this.httpClient.post<Transferencia>(this.url, transferencia);
 
     this.transferencias.push(transferencia);
   }
