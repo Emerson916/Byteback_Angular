c34e858e04033f6b2747dd1620bec62d278675f5
Emerson Almeida <emerson.almeida@adviseu.com.br>
2022-04-14 09:43:35 -0300

    exibindo os valores na tela

--- src/app/extrato/extrato.component.ts
+++ src/app/extrato/extrato.component.ts
@@ -1,5 +1,6 @@
 import { TransferenciaService } from './../services/transferencia.service';
 import { Component, Input, OnInit } from '@angular/core';
+import { Transferencia } from 'src/models/transferencia.models';
 
 @Component({
   selector: 'app-extrato',
@@ -12,7 +13,10 @@ export class ExtratoComponent implements OnInit {
 
   constructor(private service: TransferenciaService) {}
 
-  ngOnInit(): void {
-    this.transferencias = this.service.transferencias;
+  ngOnInit() {
+    this.service.todas().subscribe((transferencias: Transferencia[]) => {
+      console.table(transferencias);
+      this.transferencias = transferencias;
+    });
   }
 }
