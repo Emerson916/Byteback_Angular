06facf128e2b96dac5a3c3727357a57984b1a046
Emerson Almeida <emerson.almeida@adviseu.com.br>
2022-04-14 09:52:10 -0300

    Solicitando uma nova transferência, mudando para tela de extrato ao enviar os dados

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { Transferencia } from '../../models/transferencia.models';
+import { TransferenciaService } from './../services/transferencia.service';
 
 @Component({
   selector: 'nova-transferencia',
@@ -11,13 +14,26 @@ export class NovaTransferenciaComponent {
   valor: number;
   destino: number;
 
+  constructor(private service: TransferenciaService, private router: Router) {}
+
   transferir() {
     console.log('Solicitada uma nova tranferencia');
     console.log('valor:', this.valor);
     console.log('destino:', this.destino);
-    const valorEmitir = { valor: this.valor, destino: this.destino };
-    this.aoTransferir.emit(valorEmitir);
-    this.limparCampos();
+    const valorEmitir: Transferencia = {
+      valor: this.valor,
+      destino: this.destino,
+    };
+
+    this.service.adicionar(valorEmitir).subscribe(
+      (resultado) => {
+        console.log(resultado);
+        this.limparCampos();
+        this.router.navigateByUrl('extrato');
+      },
+      (error) => console.error(error)
+    );
+    // this.aoTransferir.emit(valorEmitir);
   }
 
   limparCampos() {
